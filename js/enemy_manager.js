38a90d27ae4b8a51c3dff83544b8659b1e8572a5
bmetcher <brendan.metcher@griffithuni.edu.au>
2025-11-03 18:54:45 +0800

    add(enemy): enemy manager with grunt now

---
+++ js/enemy_manager.js
@@ -0,0 +1,96 @@
+import { tad, make, keys, time, math } from "../../lib/TeachAndDraw.js";
+import { RepeatingTimer } from "./timer.js";
+
+export class EnemyManager {
+    constructor(unit) {
+        this.unit = unit;
+        this.gap = this.width/2 + 40;
+        this.all = make.group();    // use this maybe later?
+
+        this.grunts = make.group();
+
+        // temporary storing set enemy values
+        this.spec = {
+            "grunt": {
+                height: unit/2,
+                width: unit/2
+            }
+        }
+        this.grunt_timer = new RepeatingTimer(5);
+        this.grunt_timer.start();
+    }
+
+    update() {
+        this.dev_controls();
+        // spawn wave of enemies periodically
+        if (time.seconds%5 === 0 && this.spawning === false) {
+            this.spawn_wave();
+            this.spawning = true;
+            //console.log(this.grunts);
+        } else if (time.seconds%5 !== 0) {
+            this.spawning = false;
+        }
+
+        this.grunts.collides(this.grunts);
+        this.grunt_movement();
+
+        this.grunts.draw();
+    }
+
+    dev_controls() {
+        if (keys.released("G")) {
+            this.make_enemy(70, 70, "grunt");
+        }
+    }
+
+    spawn_wave() {
+        console.log("Spawned wave");
+        let wave_size = 5;
+        for (let i = 0; i < wave_size; i++) {
+            let gap = i * 50;
+            let random_x = 50 + (i / wave_size) * tad.w;
+            let random_y = 0;
+            this.make_enemy(random_x, random_y, "grunt");
+        }
+    }
+
+    make_enemy(x, y, type) {
+        let spec = this.spec[type];
+        let temp = make.boxCollider(x, y, spec.height, spec.width);
+        temp.direction = 180;
+        temp.speed = 10;
+        temp.friction = 0.5;
+        temp.lifespan = 8;
+        if (type === "grunt") {
+            this.all.push(temp);
+            this.grunts.push(temp);
+        }
+    }
+
+    grunt_movement() {
+        for (let i = 0; i < this.grunts.length; i++) {
+            if (this.grunts[i].speed < 1) {
+                this.grunts[i].speed = 5;
+                if (this.grunts[i].y > tad.h/3 && this.grunts[i].x > tad.w/2) {
+                    // bottom right quadrant
+                    this.grunts[i].direction = 270 + (math.random(0, 1) * 90);
+                } else if (this.grunts[i].y > tad.h/3 && this.grunts[i].x < tad.w/2) {
+                    // above threshold -> move down
+                    this.grunts[i].direction = 90 + (math.random(0, 1) * 180);
+                }
+
+
+            }
+
+            // ---- Keep Grunt Within Bounds ----
+            if (this.grunts[i].x < this.gap){
+                this.grunts[i].direction = 90;
+                
+            }
+            if (this.grunts[i].x > tad.w - this.gap){ 
+                this.grunts[i].direction = 270;
+                console.log("Changing direction");
+            }
+        }
+    }
+}
