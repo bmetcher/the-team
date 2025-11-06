93efdd979ced78df114c33afabb88debb69cf2da
Thomfee <thomas.knight55327@gmail.com>
2025-11-06 17:55:57 +1000

    Created load.js file for loading assets into main

---
+++ js/load.js
@@ -0,0 +1,63 @@
+import {tad, load} from "../lib/TeachAndDraw.js";
+
+// ---- Preload Assets ----
+// Player images
+const all_players_images = {
+    default_ship: load.image(0,0,"./images/player/default_ship.png"),
+    tank_ship: load.image(0,0,"./images/player/tank_ship.png")  // Added the ships here with correct names
+}
+
+// Ammo images
+const all_ammo_images = {
+    bullet: load.image(0,0,"./images/ammo/bullet.png"),
+    missile: load.image(0,0,"./images/ammo/missile.png")
+}
+
+// Enemy images
+const all_enemy_images = {
+    grunt: load.image(0,0, "./images/enemies/grunt.png"),
+    enemy2: load.image(0,0, "./images/enemies/enemy2.png"),
+}
+// Environment images
+const all_environment_images = {
+    space1: load.image(tad.w/2, 0, "./images/background/space1.jpeg"),
+    space2: load.image(tad.w/2, tad.h, "./images/background/space2.jpeg")
+}
+
+// Animations
+const all_explosions = {
+    player: load.animation(0,0,
+        "./images/explosions/player_explosion_animation/step_1.png",
+        // "./images/explosions/player_explosion_animation/step_2.png",
+        // "./images/explosions/player_explosion_animation/step_3.png",
+        // "./images/explosions/player_explosion_animation/step_4.png",
+        // "./images/explosions/player_explosion_animation/step_5.png",
+        // "./images/explosions/player_explosion_animation/step_6.png",
+        // "./images/explosions/player_explosion_animation/step_7.png",
+        // "./images/explosions/player_explosion_animation/step_8.png"
+    ),
+    grunt: load.animation(0,0,
+        "./images/explosions/enemy_explosion_animation/step_1.png",
+        "./images/explosions/enemy_explosion_animation/step_2.png",
+        "./images/explosions/enemy_explosion_animation/step_3.png",
+        "./images/explosions/enemy_explosion_animation/step_4.png",
+        // "./images/explosions/enemy_explosion_animation/step_5.png",
+        // "./images/explosions/enemy_explosion_animation/step_6.png",
+        // "./images/explosions/enemy_explosion_animation/step_7.png",
+        // "./images/explosions/enemy_explosion_animation/step_8.png"
+    )
+}
+
+// Files
+    const all_ammo_data = load.json("./data/ammo_map.json");
+    const all_ship_data = load.json("./data/ships_map.json"); // Seperated into seperate JSON files
+    const all_enemies_data = load.json("./data/enemies_map.json");
+
+const assets = { 
+    all_players_images, all_ammo_images, 
+    all_explosions,
+    all_enemy_images, all_environment_images,
+    all_ammo_data, all_ship_data, all_enemies_data
+};
+
+export {assets};
--- js/main.js
+++ js/main.js
@@ -3,11 +3,23 @@ import { EnemyManager } from "./enemy_manager.js";
 import { EnvironmentManager } from "./environment_manager.js";
 import { PlayerManager } from "./player_manager.js";
 import { ProjectileManager } from "./projectile_manager.js";
+import { assets } from "./load.js";
 
-
+// ---- Game Size ----
 const unit = 64;
-tad.w = 640;
-tad.h = 640;
+tad.w = 720;
+tad.h = 1080;
+
+// ---- Preload Assets ----
+const all_players_images = assets.all_players_images;
+const all_ammo_images = assets.all_ammo_images;
+const all_explosions = assets.all_explosions;
+// const all_effects = assets.all_effects;
+const all_enemy_images = assets.all_enemy_images;
+const all_environment_images = assets.all_environment_images;
+const all_ammo_data = assets.all_ammo_data;
+const all_ship_data = assets.all_ship_data;
+const all_enemy_data = assets.all_enemies_data;
 
 
 // ---- Define Screens ----
@@ -22,62 +34,6 @@ const END_GAME = 7;     // after game ends, display encouraging message and scor
 let game_state = LOADING;
 
 
-// ---- Preload Assets ----
-// Player images
-const all_players_images = {
-    default_ship: load.image(0,0,"./images/player/default_ship.png"),
-    tank_ship: load.image(0,0,"./images/player/tank_ship.png")  // Added the ships here with correct names
-}
-
-// Ammo images
-const all_ammo_images = {
-    bullet: load.image(0,0,"./images/ammo/bullet.png"),
-    missile: load.image(0,0,"./images/ammo/missile.png")
-}
-
-// Enemy images
-const all_enemy_images = {
-    grunt: load.image(0,0, "./images/enemies/grunt.png"),
-    enemy2: load.image(0,0, "./images/enemies/enemy2.png"),
-}
-// Environment images
-const all_environment_images = {
-    space1: load.image(tad.w/2, 0, "./images/background/space1.jpeg"),
-    space2: load.image(tad.w/2, tad.h, "./images/background/space2.jpeg")
-}
-
-// Animations
-const all_explosions = {
-    player: load.animation(0,0,
-        "./images/explosions/player_explosion_animation/step_1.png",
-        // "./images/explosions/player_explosion_animation/step_2.png",
-        // "./images/explosions/player_explosion_animation/step_3.png",
-        // "./images/explosions/player_explosion_animation/step_4.png",
-        // "./images/explosions/player_explosion_animation/step_5.png",
-        // "./images/explosions/player_explosion_animation/step_6.png",
-        // "./images/explosions/player_explosion_animation/step_7.png",
-        // "./images/explosions/player_explosion_animation/step_8.png"
-    ),
-    grunt: load.animation(0,0,
-        "./images/explosions/enemy_explosion_animation/step_1.png",
-        "./images/explosions/enemy_explosion_animation/step_2.png",
-        "./images/explosions/enemy_explosion_animation/step_3.png",
-        "./images/explosions/enemy_explosion_animation/step_4.png",
-        // "./images/explosions/enemy_explosion_animation/step_5.png",
-        // "./images/explosions/enemy_explosion_animation/step_6.png",
-        // "./images/explosions/enemy_explosion_animation/step_7.png",
-        // "./images/explosions/enemy_explosion_animation/step_8.png"
-    )
-}
-
-// Files
-const files = {
-    all_ammo_data: load.json("./data/ammo_map.json"),
-    all_ship_data: load.json("./data/ships_map.json"),  // Seperated into seperate JSON files
-    all_enemies_data: load.json("./data/enemies_map.json")
-}
-
-
 // Declare manager variables
 let environment, player, enemies, projectiles;
 
@@ -86,7 +42,7 @@ function initial_setup() {
 
     if (time.frameCount === 0) {
         // check if all JSON assets are loaded
-        if (files.all_ship_data && files.all_ammo_data) {
+        if (all_ship_data && all_ammo_data) {
             // Create managers AFTER data has loaded
             console.log("Assets loaded! Initializing game...");
 
@@ -94,11 +50,11 @@ function initial_setup() {
             environment = new EnvironmentManager(unit, all_environment_images);
 
             // ---- Initialise Player and Enemies ----
-            player = new PlayerManager("default_ship", all_players_images, files.all_ship_data);  // updated for new parameters
-            enemies = new EnemyManager(all_enemy_images, files.all_enemies_data);
+            player = new PlayerManager("default_ship", all_players_images, all_ship_data);  // updated for new parameters
+            enemies = new EnemyManager(all_enemy_images, all_enemy_data);
 
             // ---- Initialize Projectiles -----
-            projectiles = new ProjectileManager(unit, files.all_ammo_data, files.all_enemies_data, all_ammo_images, all_explosions);
+            projectiles = new ProjectileManager(unit, all_ammo_data, all_enemy_data, all_ammo_images, all_explosions);
 
             // ?? Set game state here ??
             // game_state = MAIN_MENU;
