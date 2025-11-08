fef5578a3191706e00ea1168b50a8b5e8da32780
bmetcher <brendan.metcher@griffithuni.edu.au>
2025-11-08 22:33:25 +0800

    and the enemy hp bug lol

--- js/projectile_manager.js
+++ js/projectile_manager.js
@@ -137,7 +137,7 @@ export class ProjectileManager {
     // Calculate damage for some target that was hit by a projectile
     damage_target(target, projectile) {
         // Handle if the target should be dead
-        if ((target.current_hp -= projectile.damage) <= 0) {
+        if ((target.current_hp - projectile.damage) <= 0) {
             target.remove();
             if (target.score > 0) {
             this.player_score += target.score;
